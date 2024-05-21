387425425d5306558c72899fa52967fce60193ef
Varun Subramanyan <varun.subramanyan@gmail.com>
2024-05-21 14:28:51 -0700

    Update vars

--- server/app/core/config.py
+++ server/app/core/config.py
@@ -11,7 +11,16 @@ class Settings(BaseSettings):
     TO_EMAIL: str
     FRONTEND_URL: str
     BUCKET_NAME: str
-    GOOGLE_APPLICATION_CREDENTIALS: str
+    GCP_TYPE: str
+    GCP_PROJECT_ID: str
+    GCP_PRIVATE_KEY_ID: str
+    GCP_PRIVATE_KEY: str
+    GCP_CLIENT_EMAIL: str
+    GCP_CLIENT_ID: str
+    GCP_AUTH_URI: str
+    GCP_TOKEN_URI: str
+    GCP_AUTH_PROVIDER_X509_CERT_URL: str
+    GCP_CLIENT_X509_CERT_URL: str
 
     class Config:
         env_file = ".env"
