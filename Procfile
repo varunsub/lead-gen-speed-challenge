cf77d4dbe6c7a3666e04a012961bb23af0cd228a
Varun Subramanyan <varun.subramanyan@gmail.com>
2024-05-21 08:00:31 -0700

    Add deployment files and scripts

---
+++ Procfile
@@ -0,0 +1,3 @@
+release: echo "Release phase script running"
+web: npm --prefix frontend start
+backend: uvicorn backend.app.main:app --host 0.0.0.0 --port 8000
---
+++ create_gcp_service_key.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Creating GCP service key JSON file..."
+
+cat <<EOF > /server/credentials.json
+{
+  "type": "${GCP_TYPE}",
+  "project_id": "${GCP_PROJECT_ID}",
+  "private_key_id": "${GCP_PRIVATE_KEY_ID}",
+  "private_key": "${GCP_PRIVATE_KEY}",
+  "client_email": "${GCP_CLIENT_EMAIL}",
+  "client_id": "${GCP_CLIENT_ID}",
+  "auth_uri": "${GCP_AUTH_URI}",
+  "token_uri": "${GCP_TOKEN_URI}",
+  "auth_provider_x509_cert_url": "${GCP_AUTH_PROVIDER_X509_CERT_URL}",
+  "client_x509_cert_url": "${GCP_CLIENT_X509_CERT_URL}"
+}
+EOF
+
+echo "GCP service key JSON file created at /server/credentials.json"
